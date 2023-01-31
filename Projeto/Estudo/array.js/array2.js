03de18dfe42f23515e1f57482f21ec32f1ed8639
Esdras <esdrasabbiu@gmail.com>
2023-01-30 23:28:46 -0300

    ...

--- Projeto/Estudo/array.js/array2.js
+++ Projeto/Estudo/array.js/array2.js
@@ -1,2 +1,26 @@
 const pilotos = ['Vettel', 'Alsonso', 'Raikkonen', 'Massa']
-//passei o dia jogando mine e esqueci de repor de manha, amanha termino o estudo
+pilotos.pop() 
+console.log(pilotos)
+
+pilotos.push('Verstappen')
+console.log(pilotos)
+
+pilotos.shift()
+console.log(pilotos)
+
+pilotos.unshift('Hamilton')
+console.log(pilotos)
+
+//splice add/rem valores
+
+pilotos.splice(2, 0, 'Bottas', 'Massa')
+console.log(pilotos)
+
+pilotos.splice(3, 1)
+console.log(pilotos)
+
+const algunsPilotos = pilotos.slice(2)//retorna um novo array
+console.log(algunsPilotos)
+
+const algunsPilotos2 = pilotos.slice(1, 4)
+console.log(algunsPilotos2)
