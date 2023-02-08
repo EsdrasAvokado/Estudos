a89dc926da71e10e5bee16c780af28d01232595b
Esdras <esdrasabbiu@gmail.com>
2023-02-07 23:12:07 -0300

    ...

---
+++ Projeto/Estudo/array.js/pizza.js
@@ -0,0 +1,11 @@
+const pizza = [
+    {nome: 'Pizza Toquio', valor: 58, sabor: 'Variavel'},
+    {nome: 'Pizza de Frango com Bacon', valor: 68, sabor:'Variavel'}
+]
+
+const precoMenor = function(pizza){
+    if(pizza.valor <= 50)
+    return pizza
+}
+
+console.log(pizza.filter(precoMenor))
