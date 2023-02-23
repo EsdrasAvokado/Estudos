93c266eae67496b18e5e3593619e37060d745d54
Esdras <esdrasabbiu@gmail.com>
2023-02-22 23:20:54 -0300

    ...

--- MEnsagem.md
+++
@@ -1,2 +0,0 @@
-//mensagem para minha pessoa, lembre-se do dia 16/02/2023 do acontecimento na escola, conversa, momento "felizinho"
-
---
+++ Projeto/Estudo/node/moduloA.js
@@ -0,0 +1,4 @@
+console.log(this)
+this.ola = 'Fala pessoal'
+exports.bemVindo = 'Seja Bem-Vindo'
+module.exports.ateLogo = 'Ate logo'
