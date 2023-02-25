57174752eb9e0f8a98da22aee662f6986a4ac31c
Esdras <esdrasabbiu@gmail.com>
2023-02-24 23:26:34 -0300

    ...

--- Projeto/Estudo/node/pastaA/pastaB/acessandoModulos.js
+++ Projeto/Estudo/node/pastaA/pastaB/acessandoModulos.js
@@ -1,2 +1,15 @@
 const moduloA = require('../../moduloA')//moduloa
 console.log(moduloA.ola)
+
+const saudacao = require('saudacao')
+console.log(saudacao.ola)
+
+const http = require('http')
+
+http.createServer((req, res) => {
+    res.write('Bom Dia')
+    res.end()
+}).listen(8080)
+
+const c = require('./pastaC/index')
+console.log(c.ola2)
