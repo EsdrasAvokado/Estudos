5a766f8f33e5f722af5db0852581cf11a8592fc6
Esdras <esdrasabbiu@gmail.com>
2023-03-08 22:56:48 -0300

    ...

--- Projeto/Estudo/node/moduloCliente.js
+++ Projeto/Estudo/node/moduloCliente.js
@@ -1,5 +1,5 @@
-const moduloA = require('./moduloA')
-const moduloB = require('./moduloB')
+const moduloA = require( './moduloA' )
+const moduloB = require( './moduloB' )
 
 console.log(moduloA.ola)
 console.log(moduloA)
