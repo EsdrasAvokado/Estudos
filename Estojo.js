e08ebc1a0ca7ab10a0570fdb3e5a46fedec6e0f2
Esdras <esdrasabbiu@gmail.com>
2023-03-16 23:01:43 -0300

    ...

---
+++ Estojo.js
@@ -0,0 +1,25 @@
+const estojo = {
+    compartimentoEsquerdo: {
+    lapis: {
+        lapisColoridos: 34,
+        lapisEscrita: 6,
+        canetas: 5,
+        canetinhas: 4,
+    }},
+    compartimentoDireito: { 
+    itens: {
+        borracha: 2,
+        tessoura: 1,
+        apontador: 3,
+        caixaDeMunicaoGrampeador: 1,
+        regua: 2,
+        grampeador: 1,
+        colaBastao: 2
+    }          
+    }
+}
+
+console.log(estojo)
+
+const cores = '2 lapis marrom, 2 lapis preto, 2 rosas, 2 laranjas, 2 azul e 1 forte, 2 roxo, 2 amarelo, 1 amarelo forte, 2 cor de pele, 2 verde, 2 verde fraco, 2 vermelho, 2 azul do mar'
+console.log(cores)
